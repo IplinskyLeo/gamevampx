4cd9cbf236a078686148bb1bbf54bddad540ca25
IplinskyLeo <leoipy16@gmail.com>
2022-05-04 11:37:17 -0300

    carlinhos finalizado backgorund arrumado

---
+++ javascript/carlinhos2.js
@@ -0,0 +1,19 @@
+// fase 1 carlinhos r: 1
+function btnResposta ( ) {
+    var resposta = prompt("Escolha 1 ou 2");
+    while (true) {
+        if(resposta == 1) {
+            window.location.href = "/html/personagens/carlinhos/carlinhos_3.html";
+            break; 
+        }
+        else if (resposta == 2) {
+            alert ('Escolha errada, Game Over!');
+            window.location.href = "/html/creditos.html";
+            break;
+        }
+        else {
+            alert('Escolha errada. Digite 1 ou 2.');
+            break;
+        }
+        }
+}
---
+++ javascript/carlinhos3.js
@@ -0,0 +1,19 @@
+// fase 1 carlinhos r: 1
+function btnResposta ( ) {
+    var resposta = prompt("Escolha 1 ou 2");
+    while (true) {
+        if(resposta == 2) {
+            window.location.href = "/html/win.html";
+            break; 
+        }
+        else if (resposta == 1) {
+            alert ('Escolha errada, Game Over!');
+            window.location.href = "/html/creditos.html";
+            break;
+        }
+        else {
+            alert('Escolha errada. Digite 1 ou 2.');
+            break;
+        }
+        }
+}
